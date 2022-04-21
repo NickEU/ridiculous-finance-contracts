80b896280f5984e5cef08bf1f9041d9deca3c397
Nikolajs Korolkovs <nikolajs.korolkovs@protonmail.com>
2022-04-21 12:47:16 +0300

    test(pools): added config file for solidity coverage

---
+++ pools/.solcover.js
@@ -0,0 +1,16 @@
+module.exports = {
+  skipFiles: [
+    "libraries/Babylonian.sol",
+    "libraries/Math.sol",
+    "libraries/RidiLibrary.sol",
+    "libraries/SafeMath.sol",
+    "libraries/UQ112x112.sol",
+    "libraries/WFTM.sol",
+    "RidiERC20.sol",
+    "RidiFactory.sol",
+    "RidiPair.sol",
+    "RidiRouter.sol",
+    "RidiRouter01.sol",
+    "utils/MockERC20.sol",
+  ],
+};
